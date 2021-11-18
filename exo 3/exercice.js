deaebd8264d4992cab02f9411a186b1e8a950de1
Lyz03 <lizoe@netcourrier.com>
2021-11-18 09:37:19 +0100

    part 3

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,4 +1,9 @@
+let div = document.getElementsByClassName('name-tag');
 
+for (let i = 0; i < div.length; i++) {
+    let text = document.createElement('p').innerText = 'HELLO WORLD !';
+    div[i].prepend(text);
+}
 
 
 

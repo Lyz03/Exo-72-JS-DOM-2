77caf399fd74a9e5cdc89222d3814b4a36526088
Lyz03 <lizoe@netcourrier.com>
2021-11-18 09:34:02 +0100

    part 2

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -1,4 +1,10 @@
+let div = document.getElementsByClassName('name-tag');
 
+for (let i = 0; i < div.length; i++) {
+
+    let text = document.createElement('span').innerText = 'Bonjour, je suis';
+    div[i].prepend(text);
+}
 
 
 
